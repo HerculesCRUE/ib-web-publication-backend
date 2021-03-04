65097fbc09d0c91ef3fd950a90a20f5b3e381e0d
pteran <pteran@izertis.com>
2021-03-04 13:07:28 +0100

    [SEMANTMURC-581] Dejar preparado back y front para dar soporte a consultas federadas Useless code deleted and call to fuseki is in back

--- web-publication-backend-service/src/main/java/es/um/asio/service/service/sparql/impl/SparqlExecQueryImpl.java
+++ web-publication-backend-service/src/main/java/es/um/asio/service/service/sparql/impl/SparqlExecQueryImpl.java
@@ -3,9 +3,6 @@
  */
 package es.um.asio.service.service.sparql.impl;
 
-import java.net.URLEncoder;
-import java.net.http.HttpRequest;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -271,17 +268,4 @@ public class SparqlExecQueryImpl implements SparqlExecQuery {
 		return body;
 	}
 
-	private static HttpRequest.BodyPublisher buildFormDataFromMap(Map<Object, Object> data) {
-		var builder = new StringBuilder();
-		for (Map.Entry<Object, Object> entry : data.entrySet()) {
-			if (builder.length() > 0) {
-				builder.append("&");
-			}
-			builder.append(URLEncoder.encode(entry.getKey().toString(), StandardCharsets.UTF_8));
-			builder.append("=");
-			builder.append(URLEncoder.encode(entry.getValue().toString(), StandardCharsets.UTF_8));
-		}
-		System.out.println(builder.toString());
-		return HttpRequest.BodyPublishers.ofString(builder.toString());
-	}
 }
