f73534caeb0fc433a4c5747d970ec72f9617b501
Atorrecilla <atorrecilla@izertis.com>
2021-03-22 14:23:38 +0100

    SEMANTMURC-619

--- web-publication-backend-service/src/main/java/es/um/asio/service/dto/OrganizationDetailDto.java
+++ web-publication-backend-service/src/main/java/es/um/asio/service/dto/OrganizationDetailDto.java
@@ -3,79 +3,82 @@ package es.um.asio.service.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-
 /**
  * The Class OrganizationDto.
  */
 
-
 @Getter
 @Setter
 public class OrganizationDetailDto {
-	
+
 	/** The id. */
 	private String id;
-	
+
 	/** The title. */
 	private String title;
-	
+
 	/** The abbreviation. */
 	private String abbreviation;
-	
+
 	/** The description. */
 	private String description;
-	
+
 	/** The endDate. */
 	private String endDate;
-	
+
 	/** The homepage. */
 	private String homepage;
-	
+
 	/** The isStartup. */
 	private String isStartup;
-	
+
 	/** The publicCompany. */
 	private String publicCompany;
-	
+
 	/** The startDate. */
 	private String startDate;
-	
+
 	/** The has knowledge area. */
 	private String hasKnowledgeArea;
-	
+
 	/** The has contact info. */
 	private String hasContactInfo;
-	
+
 	/** The provides. */
 	private String provides;
-	
+
 	/** The located in. */
 	private String locatedIn;
-	
+
 	/** The has acreditation. */
 	private String hasAcreditation;
-	
+
 	/** The has predecessor organization. */
 	private String hasPredecessorOrganization;
-	
+
 	/** The contains. */
 	// http://purl.org/roh/mirror/obo/ro#BFO_0000051
 	private String contains;
-	
+
 	/** The date time interval. */
 //	private String dateTimeInterval;
-	
+
 	private String participates;
-	
+
 	/** The has successor organization. */
 	private String hasSuccessorOrganization;
-	
+
 	/** The has reservable. */
 	private String hasReservable;
-	
+
 	/** The freetext keyword. */
 	private String freetextKeyword;
-	
+
 	/** The related by. */
 	private String relatedBy;
+
+	/**
+	 * The type
+	 */
+	private String type;
 }
