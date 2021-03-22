50d71f01d5b0b05ab1f34847cd5a0fb2ac37a47c
pteran <pteran@izertis.com>
2021-03-22 15:07:13 +0100

    [SEMANTMURC-625] Mock servicio patentes Mock servicio patentes

--- web-publication-backend-back/src/main/java/es/um/asio/back/controller/patent/PatentController.java
+++ web-publication-backend-back/src/main/java/es/um/asio/back/controller/patent/PatentController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.um.asio.service.dto.PatentDetailDto;
 import es.um.asio.service.dto.PatentDto;
 import es.um.asio.service.filter.patent.PatentFilter;
 import es.um.asio.service.proxy.patent.PatentProxy;
@@ -50,6 +52,11 @@ public class PatentController {
 	public Page<PatentDto> searchProyects(final PatentFilter filter, final Pageable pageable) {
 		return this.proxy.findPaginated(filter, pageable);
 	}
+	
+	@GetMapping(PatentController.Mappings.GET)
+	public PatentDetailDto findPatent(@PathVariable("id") final String id) {
+		return createMockDetailDto();
+	}
 
 	@GetMapping(PatentController.Mappings.AREA)
 	public String area() {
@@ -67,11 +74,29 @@ public class PatentController {
 		 * Mapping for search.
 		 */
 		protected static final String SEARCH = "/search";
+		
+		
+		/**
+		 * Mapping for get.
+		 */
+		protected static final String GET = "/{id}";
 
 		/**
 		 * Graphics area.
 		 */
 		protected static final String AREA = "/area";
+	}
+	
+	private PatentDetailDto createMockDetailDto() {
+		PatentDetailDto mock = new PatentDetailDto();
+		mock.setTitle("FUNCIONALIZACIÓN DE PARTÍCULAS MAGNÉTICAS MEDIANTE SUSTITUCIÓN NUCLEOFÍLICA DE HALUROS ORGÁNICOS");
+		mock.setDateIssued("2016-04-06");
+		mock.setEndDate("2016-05-06");
+		mock.setId("123");
+		mock.setKeyword("magnetismo");
+		mock.setStartDate("2015-04-06");
+		mock.setSummary("FUNCIONALIZACIÓN DE PARTÍCULAS MAGNÉTICAS MEDIANTE SUSTITUCIÓN NUCLEOFÍLICA DE HALUROS ORGÁNICOS");
+		return mock;
 	}
 
 }
---
+++ web-publication-backend-service/src/main/java/es/um/asio/service/dto/PatentDetailDto.java
@@ -0,0 +1,97 @@
+package es.um.asio.service.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PatentDto.
+ */
+
+/**
+ * Gets the type.
+ *
+ * @return the type
+ */
+
+/**
+ * Gets the type.
+ *
+ * @return the type
+ */
+
+/**
+ * Gets the type.
+ *
+ * @return the type
+ */
+
+/**
+ * Gets the type.
+ *
+ * @return the type
+ */
+@Getter
+
+/**
+ * Sets the type.
+ *
+ * @param type the new type
+ */
+
+/**
+ * Sets the type.
+ *
+ * @param type the new type
+ */
+
+/**
+ * Sets the type.
+ *
+ * @param type the new type
+ */
+
+/**
+ * Sets the type.
+ *
+ * @param type the new type
+ */
+@Setter
+public class PatentDetailDto {
+	
+	/** The id. */
+	private String id;
+
+	/** The title. */
+	private String title;
+
+	/** The dateIssued. */
+	private String dateIssued;
+	
+	/** The doi. */
+	private String doi;
+	
+	/** The endDate. */
+	private String endDate;
+	
+	/** The endPage. */
+	private String endPage;
+	
+	/** The keyword. */
+	private String keyword;
+	
+	/** The mode. */
+	private String mode;
+	
+	/** The startDate. */
+	private String startDate;
+	
+	/** The startPage. */
+	private String startPage;
+	
+	/** The summary. */
+	private String summary;
+	
+	
+
+}
