47e0bfa251ef637ae3928585a28d0e7581a78902
pteran <pteran@izertis.com>
2021-03-24 16:25:35 +0100

    added filter to conference

--- web-publication-backend-service/src/main/java/es/um/asio/service/filter/conference/ConferenceFilter.java
+++ web-publication-backend-service/src/main/java/es/um/asio/service/filter/conference/ConferenceFilter.java
@@ -5,61 +5,81 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+// TODO: Auto-generated Javadoc
 /**
  * The Class CongressFilter.
  */
+
+/**
+ * Gets the participan id.
+ *
+ * @return the participan id
+ */
+
+/**
+ * Gets the participan id.
+ *
+ * @return the participan id
+ */
 @Getter
+
+/**
+ * Sets the participan id.
+ *
+ * @param participanId the new participan id
+ */
+
+/**
+ * Sets the participan id.
+ *
+ * @param participanId the new participan id
+ */
 @Setter
+
+/**
+ * To string.
+ *
+ * @return the java.lang. string
+ */
+
+/**
+ * To string.
+ *
+ * @return the java.lang. string
+ */
 @ToString(includeFieldNames = true)
 public class ConferenceFilter extends Filter {
 	
-	/**
-	 * 
-	 */
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -8763345133106144921L;
 	
-	/**
-	 * The abbreviation
-	 */
+	/** The abbreviation. */
 	private String abbreviation;
 	
-	/**
-	 * The contactInformation
-	 */
+	/** The contactInformation. */
 	private String contactInformation;
 	
-	/**
-	 * The date
-	 */
+	/** The date. */
 	private String date;
 	
-	/**
-	 * The description
-	 */
+	/** The description. */
 	private String description;
 	
-	/**
-	 * The id
-	 */
+	/** The id. */
 	private String id;
 	
-	/**
-	 * The locality
-	 */
+	/** The locality. */
 	private String locality;
 	
-	/**
-	 * The locatedIn
-	 */
+	/** The locatedIn. */
 	private String locatedIn;
 	
-	/**
-	 * The presents
-	 */
+	/** The presents. */
 	private String presents;
 	
-	/**
-	 * The title
-	 */
+	/** The title. */
 	private String title;
+
+	/** The participant id. */
+	private String participantId;
 }
