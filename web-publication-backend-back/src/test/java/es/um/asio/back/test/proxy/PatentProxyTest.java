7dbf24d768ab5730627086eb05d58f9047ac5a0c
Rubén Gavilán <rgavilan@izertis.com>
2021-07-15 19:38:50 +0200

    fix tests

--- web-publication-backend-back/src/test/java/es/um/asio/back/test/proxy/PatentProxyTest.java
+++ web-publication-backend-back/src/test/java/es/um/asio/back/test/proxy/PatentProxyTest.java
@@ -2,11 +2,18 @@ package es.um.asio.back.test.proxy;
 
 import static org.junit.Assert.assertNotNull;
 
+import es.um.asio.back.test.mock.MockMailSender;
+import es.um.asio.service.mapper.decorator.ArticleMapperDecorator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -26,6 +33,17 @@ public class PatentProxyTest {
 
 	@MockBean
 	private PatentService service;
+
+	@TestConfiguration
+	static class PatentProxyTestConfiguration {
+
+		@Bean
+		public JavaMailSender mailSender() {
+			final JavaMailSenderImpl sender;
+			sender = new MockMailSender();
+			return sender;
+		}
+	}
 
 	@Test
 	public void test_Infraestructure() {
