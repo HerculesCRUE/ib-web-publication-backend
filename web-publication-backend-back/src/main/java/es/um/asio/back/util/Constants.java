37230ad58d5a27636f5ae85c1f604f4f0f2d4579
Hugo Pintado <hpintado@izertis.com>
2021-11-17 17:29:05 +0100

    envio de correos

--- web-publication-backend-back/src/main/java/es/um/asio/back/util/Constants.java
+++ web-publication-backend-back/src/main/java/es/um/asio/back/util/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
 	public static final String IMPORT = "IMPORT";
 	public static final String SUBJECT_IMPORT = "Finalizado el proceso de importación";
-	public static final String EMAIL_BODY_IMPORT = "";
+	public static final String EMAIL_BODY_IMPORT = "Finalizado el proceso de importación";
 
 }
--- web-publication-backend-service/src/main/java/es/um/asio/service/service/mail/impl/EmailServiceImp.java
+++ web-publication-backend-service/src/main/java/es/um/asio/service/service/mail/impl/EmailServiceImp.java
@@ -3,6 +3,7 @@ package es.um.asio.service.service.mail.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,9 @@ public class EmailServiceImp implements EmailService {
 
 	@Autowired(required = true)
 	private SparqlQueryRepository repository;
+	
+	 @Value("${spring.mail.username}")
+	private String from;
 
 	@Override
 	public void sendSimpleMail(List<String> tos, String subject, String text) {
@@ -25,7 +29,7 @@ public class EmailServiceImp implements EmailService {
 		for (String to : tos) {
 			msg.setTo(to);
 		}
-
+		msg.setFrom(from);
 		msg.setSubject(subject);
 		msg.setText(text);
 		javaMailSender.send(msg);
